9584383d071695d043669f76fd16f378f5cc889f
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-06-05 02:56:01 +0600

    Update images.js

--- images.js
+++ images.js
@@ -1,16 +1,11 @@
 var postInfoo = [
-   { filename: 'post1', date: '04-03-2024' },
-   { filename: 'post2', date: '09-03-2024' },
-   { filename: 'post3', date: '08-03-2024' },
-   { filename: 'post4', date: '07-03-2024' },
-   { filename: 'post5', date: '06-03-2024' },
-   { filename: 'post6', date: '05-03-2024' },
-   { filename: 'post7', date: '10-03-2024' },
-   { filename: 'about_eee', date: '12-03-2024' },
-   { filename: 'my_ramadan_routine', date: '12-03-2024' },
-   
- { filename: 'susteee', date: '13-03-2024' },
-   { filename: 'sust_eee_post', date: '16-03-2024' },
+   { filename: 'Posts/post1', date: '04-03-2024' },
+   { filename: 'Posts/post2', date: '09-03-2024' },
+   { filename: 'Posts/post3', date: '08-03-2024' },
+   { filename: 'Posts/post4', date: '07-03-2024' },
+   { filename: 'Posts/about_eee', date: '12-03-2024' },
+   { filename: 'Posts/susteee', date: '13-03-2024' },
+   { filename: 'Posts/sust_eee_post', date: '16-03-2024' },
 
   
    // Add more posts with their respective dates
